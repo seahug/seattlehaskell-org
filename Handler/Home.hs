a86859aa88bba2b47c502b49180f62ba03b1c0e8
Richard Cook <rcook@rcook.org>
2015-10-09 10:15:35 -0700

    Remove POST route from Home handler

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,21 +1,9 @@
-
 module Handler.Home where
 
 import Import
 import Data.Time.Format as DTF
 import Data.Time.LocalTime
-import qualified Data.Text as T
-import qualified Data.Text.Lazy as TL
-import qualified Data.Text.Format as TF
 import qualified Util.MeetupApi as M
-import Yesod.Form.Bootstrap3 (BootstrapFormLayout (..), renderBootstrap3, withSmallInput)
-
-formatEvent :: M.Event -> T.Text
-formatEvent e = TL.toStrict $ TF.format "{} {} {}" [
-        (M.eventTitle e)
-      , (M.eventUrl e)
-      , T.pack (formatZonedTime (M.eventZonedTime e))
-    ]
 
 formatZonedTime :: ZonedTime -> String
 formatZonedTime t = formatTime DTF.defaultTimeLocale "%A %-d %B %Y at %H:%M UTC%z" t
@@ -30,13 +18,6 @@ getMeetupEvents' fileName = do
 getMeetupEvents :: IO [M.Event]
 getMeetupEvents = getMeetupEvents' "config/meetup.yml"
 
-getMeetupEventDescriptions :: IO Text
-getMeetupEventDescriptions = do
-    events <- getMeetupEvents
-    return $ case events of
-        [] -> error "FAIL"
-        event : _ -> formatEvent event
-
 getNextMeetupEvent :: IO M.Event
 getNextMeetupEvent = do
     events <- getMeetupEvents
@@ -44,42 +25,11 @@ getNextMeetupEvent = do
         [] -> error "FAIL"
         event : _ -> event
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
 getHomeR :: Handler Html
 getHomeR = do
-    --(formWidget, formEnctype) <- generateFormPost sampleForm
-    --let submission = Nothing :: Maybe (FileInfo, Text)
-    --    handlerName = "getHomeR" :: Text
-
     nextMeetupEvent <- liftIO getNextMeetupEvent
 
     defaultLayout $ do
         aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
         $(widgetFile "homepage")
 
-postHomeR :: Handler Html
-postHomeR = do
-    --((result, formWidget), formEnctype) <- runFormPost sampleForm
-    --let handlerName = "postHomeR" :: Text
-    --    submission = case result of
-    --        FormSuccess res -> Just res
-    --        _ -> Nothing
-
-    nextMeetupEvent <- liftIO getNextMeetupEvent
-
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
-
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderBootstrap3 BootstrapBasicForm $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField (withSmallInput "What's on the file?") Nothing
