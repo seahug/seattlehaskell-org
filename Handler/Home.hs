a1b76b0b56e9089f54667f900450a573a43d8582
Richard Cook <rcook@rcook.org>
2015-10-09 10:24:21 -0700

    Simply Meetup code

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -5,29 +5,24 @@ import Data.Time.Format as DTF
 import Data.Time.LocalTime
 import qualified Util.MeetupApi as M
 
+-- $TODO: Use monad transformers or something to tidy this up
+getNextMeetupEvent :: FilePath -> IO M.Event
+getNextMeetupEvent configFileName = do
+    result <- M.readMeetupSettings configFileName
+    case result of
+        Nothing -> error "FAIL"
+        Just settings -> do
+            events <- M.getMeetupEvents settings
+            return $ case events of
+                [] -> error "FAIL"
+                event : _ -> event
+
 formatZonedTime :: ZonedTime -> String
 formatZonedTime t = formatTime DTF.defaultTimeLocale "%A %-d %B %Y at %H:%M UTC%z" t
 
-getMeetupEvents' :: FilePath -> IO [M.Event]
-getMeetupEvents' fileName = do
-    result <- M.readMeetupSettings fileName
-    case result of
-        Nothing -> error "FAIL"
-        Just settings -> M.getMeetupEvents settings
-
-getMeetupEvents :: IO [M.Event]
-getMeetupEvents = getMeetupEvents' "config/meetup.yml"
-
-getNextMeetupEvent :: IO M.Event
-getNextMeetupEvent = do
-    events <- getMeetupEvents
-    return $ case events of
-        [] -> error "FAIL"
-        event : _ -> event
-
 getHomeR :: Handler Html
 getHomeR = do
-    nextMeetupEvent <- liftIO getNextMeetupEvent
+    nextMeetupEvent <- liftIO $ getNextMeetupEvent "config/meetup.yml"
 
     defaultLayout $ do
         aDomId <- newIdent
