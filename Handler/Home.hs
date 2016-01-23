3f1f0fda94bb751e332dc9ad5cd9d7b74a75744a
Richard Cook <rcook@rcook.org>
2016-01-23 14:56:08 -0800

    Fix indentation to two spaces

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -7,32 +7,32 @@ import qualified Util.Meetup as M
 
 getHomeR :: Handler Html
 getHomeR = do
-    app <- getYesod
-    let meetupApiKey = appMeetupApiKey $ appSettings app
-    event <- liftIO $ do
-        events <- M.fetchEvents meetupApiKey
-        return $ Prelude.head events
-    defaultLayout $ do
-        setTitle "SeaHUG"
-        $(widgetFile "home")
+  app <- getYesod
+  let meetupApiKey = appMeetupApiKey $ appSettings app
+  event <- liftIO $ do
+    events <- M.fetchEvents meetupApiKey
+    return $ Prelude.head events
+  defaultLayout $ do
+    setTitle "SeaHUG"
+    $(widgetFile "home")
 
 getAboutR :: Handler Html
 getAboutR = do
-    app <- getYesod
-    let gitHubUrl = appGitHubUrl $ appSettings app
-    defaultLayout $ do
-        setTitle "SeaHUG - about"
-        $(widgetFile "about")
+  app <- getYesod
+  let gitHubUrl = appGitHubUrl $ appSettings app
+  defaultLayout $ do
+    setTitle "SeaHUG - about"
+    $(widgetFile "about")
 
 getContactR :: Handler Html
 getContactR = do
-    defaultLayout $ do
-        setTitle "SeaHUG - contact"
-        $(widgetFile "contact")
+  defaultLayout $ do
+    setTitle "SeaHUG - contact"
+    $(widgetFile "contact")
 
 getPcphR :: Handler Html
 getPcphR = do
-    defaultLayout $ do
-        setTitle "SeaHUG - Parallel and concurrent programming in Haskell"
-        $(widgetFile "pcph")
+  defaultLayout $ do
+    setTitle "SeaHUG - Parallel and concurrent programming in Haskell"
+    $(widgetFile "pcph")
 
