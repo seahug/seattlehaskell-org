819b8168ad9866504307376e7007e31913369c48
Richard Cook <rcook@rcook.org>
2017-12-30 18:13:17 -0800

    Fixes #2: Use client-side Meetup API call with signed URL

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -4,19 +4,11 @@ module Handler.Home
     ) where
 
 import           Import
-import qualified Prelude
-import qualified Util.Formatting as F
-import qualified Util.Meetup as M
 
 getHomeR :: Handler Html
 getHomeR = do
     app <- getYesod
-    let meetupApiUrl = appMeetupApiUrl $ appSettings app
-        meetupApiKey = appMeetupApiKey $ appSettings app
-        meetupUrl = appMeetupUrl $ appSettings app
-    event <- liftIO $ do
-        events <- M.fetchEvents meetupApiUrl meetupApiKey
-        return $ fmap Prelude.head events
+    let meetupUrl = appMeetupUrl $ appSettings app
     defaultLayout $ do
         setTitle "SeaHUG"
         $(widgetFile "home")
